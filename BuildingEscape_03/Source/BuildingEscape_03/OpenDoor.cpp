24f1631a5a4fdd1758edd3bb5b96d1ae43c3aa79
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2018-05-12 17:27:38 +0300

    71 - BE Walls and Textures

---
+++ BuildingEscape_03/Source/BuildingEscape_03/OpenDoor.cpp
@@ -0,0 +1,41 @@
+// Learning
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+	
+	AActor* Owner = GetOwner();
+
+	FRotator Rotation = FRotator(0.0f, -80.0f, 0.0f);
+	Owner->SetActorRotation(Rotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is opened."), *ObjectName);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
